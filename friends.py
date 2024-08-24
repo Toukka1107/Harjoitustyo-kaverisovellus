6ed42b1f1fe600f5c1e161ff7eacc9ad5f180118
Toukka1107 <161489333+Toukka1107@users.noreply.github.com>
2024-08-24 17:30:23 +0300

    Edited functions check_friend_request and check_friendship

--- friends.py
+++ friends.py
@@ -10,8 +10,7 @@ def send_request(user_id, friend_id):
         db.session.execute(sql, {"sender_id": user_id, "receiver_id": friend_id})
         db.session.commit()
         return True
-    except Exception as e:
-        print(f"Virhe: {e}")
+    except:
         db.session.rollback()
         return False
 
@@ -44,13 +43,9 @@ def add_friends(user_id: int, friend_id: int):
         INSERT INTO friends (user_id, friend_id)
         VALUES (:user_id, :friend_id)
         """)
+    
     db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
-
-    sql = text("""
-        INSERT INTO friends (user_id, friend_id)
-        VALUES (:friend_id, :user_id)
-        """)
-    db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+    db.session.execute(sql, {"user_id": friend_id, "friend_id": user_id})
 
     sql = text("""
         DELETE FROM friend_requests
@@ -59,6 +54,22 @@ def add_friends(user_id: int, friend_id: int):
     db.session.execute(sql, {"sender_id": friend_id, "receiver_id": user_id})
 
     db.session.commit()
+
+def delete_friend(user_id: int, friend_id: int):
+    sql = text("""
+               DELETE FROM friends
+               WHERE user_id = :user_id AND friend_id = :friend_id
+               """)
+    
+    try:
+        db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+        db.session.execute(sql, {"user_id": friend_id, "friend_id": user_id})
+        db.session.commit()
+        return True
+    except:
+        db.session.rollback()
+        return False
+
 
 def get_all_friends(user_id: int):
     sql = text("""
@@ -77,13 +88,21 @@ def check_friend_request(user_id: int, friend_id: int):
         WHERE sender_id = :user_id AND receiver_id = :friend_id
         """)
     result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
-    return result is not None
+    if result:
+        return True
+    else:
+        return None
 
 def check_friendship(user_id: int, friend_id: int):
     sql = text("""
         SELECT 1
         FROM friends
-        WHERE (user_id = :user_id AND friend_id = :friend_id)
+        WHERE user_id = :user_id AND friend_id = :friend_id
+               OR user_id = :friend_id AND friend_id = :user_id
         """)
+
     result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
-    return result is not None
+    if result:
+        return True
+    else:
+        return None

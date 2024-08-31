7bd387fc6ab7de87a03699a3aec3d1707072905e
Toukka1107 <161489333+Toukka1107@users.noreply.github.com>
2024-08-31 17:52:10 +0300

    Users can edit or delete messages and comments and fixes
    -Users can edit or delete their messages and comments
    -CSRF vulnerability is fixed
    -Changed username character limit
    -Avatar is shown in friends

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, session, abort
 from users import login, signup, edit_profile, get_user_id, get_user_by_username, get_profile_by_user_id, get_username, check_username
-from messages import add_message, all_messages, add_comment
-from friends import send_request, add_friends, delete_friend_request, get_friend_request, get_all_friend_requests, get_all_friends, check_friend_request, check_friendship, delete_friend
+from messages import add_message, all_messages, add_comment, edit_message, delete_message, edit_comment, delete_comment
+from friends import send_request, add_friends, delete_friend_request, get_all_friend_requests, get_all_friends, check_friend_request, check_friendship, delete_friend
+import secrets
 
 def create_routes(app):
     @app.route("/")
@@ -18,6 +19,9 @@ def create_routes(app):
 
     @app.route("/send", methods=["POST"])
     def send():
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
+
         subject = request.form.get("subject")
         message_content = request.form.get("message")
         user_id = session.get("user_id")
@@ -41,6 +45,9 @@ def create_routes(app):
     
     @app.route("/comment", methods=["POST"])
     def comment():
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
+
         comment_content = request.form.get("comment")
         user_id = session.get("user_id")
         message_id = request.form.get("message_id")
@@ -56,6 +63,71 @@ def create_routes(app):
         if user_id and comment_content:
             add_comment(user_id, message_id, comment_content)
 
+        return redirect("/")
+    
+    @app.route("/edit_message/<message_id>", methods=["GET", "POST"])
+    def edit_message_route(message_id):
+        user_id = session.get("user_id")
+        if not user_id:
+            abort(403, description="Käyttäjä ei ole kirjautunut sisään")
+
+        if request.method == "POST":
+            edited_message = request.form.get("edited_message")
+            subject = request.form.get("subject")
+            errors = []
+
+            if len(subject) > 20:
+                errors.append("Otsikon maksimipituus on 20 merkkiä")
+
+            if len(edited_message) > 800:
+                errors.append("Viestin maksimipituus on 800 merkkiä")
+
+            if errors:
+                return render_template("edit_message.html", errors=errors, message_id=message_id)
+            else:
+                edit_message(message_id, subject, edited_message)
+                return redirect("/")
+        
+        return render_template("edit_message.html", message_id=message_id)
+
+    
+    @app.route("/edit_comment/<comment_id>", methods=["GET", "POST"])
+    def edit_comment_route(comment_id):
+        user_id = session.get("user_id")
+        if not user_id:
+            abort(403, description="Käyttäjä ei ole kirjautunut sisään")
+
+        if request.method == "POST":
+            edited_comment = request.form.get("edited_comment")
+            errors = []
+
+            if len(edited_comment) > 800:
+                errors.append("Kommentin maksimipituus on 800 merkkiä")
+
+            if errors:
+                return render_template("edit_comment.html", errors=errors, comment_id=comment_id)
+            else:
+                edit_comment(comment_id, edited_comment)
+                return redirect("/")
+
+        return render_template("edit_comment.html", comment_id=comment_id)
+    
+    @app.route("/delete_message/<message_id>", methods=["POST"])
+    def delete_message_route(message_id):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
+        if not session.get("user_id"):
+            redirect("/login")
+        delete_message(message_id)
+        return redirect("/")
+    
+    @app.route("/delete_comment/<comment_id>", methods=["POST"])
+    def delete_comment_route(comment_id):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
+        if not session.get("user_id"):
+            redirect("/login")
+        delete_comment(comment_id)
         return redirect("/")
     
     @app.route("/profile/<username>")
@@ -82,6 +154,7 @@ def create_routes(app):
 
     @app.route("/profile/<username>/edit", methods=["GET", "POST"])
     def edit_profile_route(username):
+
         user_id = session.get("user_id")
         if not user_id:
             abort(403, description="Käyttäjä ei ole kirjautunut sisään")
@@ -91,6 +164,8 @@ def create_routes(app):
             return render_template("profile_edit.html", error="Profiilia ei löydy", profile=None)
 
         if request.method == "POST":
+            if session.get("csrf_token") != request.form.get("csrf_token"):
+                abort(403)
             age = request.form.get("age")
             hobbies = request.form.get("hobbies")
             about_me = request.form.get("about_me")
@@ -126,6 +201,7 @@ def create_routes(app):
             if login(username, password):
                 session["username"] = username
                 session["user_id"] = get_user_id()
+                session["csrf_token"] = secrets.token_hex(16)
                 return redirect("/")
 
             return render_template("login.html", error="Väärä käyttäjätunnus tai salasana")
@@ -136,6 +212,7 @@ def create_routes(app):
     def logout():
         session.pop("username", None)
         session.pop("user_id", None)
+        session.pop("csrf_token", None)
         return redirect("/")
 
     @app.route("/signup", methods=["GET", "POST"])
@@ -153,8 +230,8 @@ def create_routes(app):
             if check_username(username):
                 errors.append("Käyttäjätunnus on jo käytössä")
 
-            if len(username) < 4 or len(username) > 20:
-                errors.append("Käyttäjätunnuksen pitää olla 4-20 merkkiä")
+            if len(username) < 4 or len(username) > 16:
+                errors.append("Käyttäjätunnuksen pitää olla 4-16 merkkiä")
             
             if len(password) < 8 or len(password) > 24:
                 errors.append("Salasanan pitää olla 8-24 merkkiä")
@@ -181,8 +258,8 @@ def create_routes(app):
                 signup(username, password, age, hobbies, about_me, avatar)
                 session["username"] = username
                 session["user_id"] = get_user_id()
+                session["csrf_token"] = secrets.token_hex(16)
                 return redirect("/")
-
 
         return render_template("signup.html")
 
@@ -194,11 +271,12 @@ def create_routes(app):
         
         requests = get_all_friend_requests(user_id)
         friends = get_all_friends(user_id)
-
         return render_template("friends.html", requests=requests, friends=friends)
         
     @app.route("/send_friend_request/<friend_id>", methods=["POST"])
     def send_friend_request(friend_id):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
         user_id = session.get("user_id")
         profile_username = get_username(friend_id)
         if not user_id:
@@ -207,13 +285,13 @@ def create_routes(app):
         if not friend_id:
             return redirect("/profile/{{ session['username'] }}", error="Virheellinen kaveripyyntö")
 
-        if send_request(user_id, friend_id):
-            return redirect(f"/profile/{profile_username}")
-        else:
-            return render_template("profile.html", error="Kaveripyyntöä ei voitu lähettää")
+        send_request(user_id, friend_id)
+        return redirect(f"/profile/{profile_username}")
         
     @app.route("/end_friendship/<friend_id>", methods=["POST"])
     def end_friendship(friend_id):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
         user_id = session.get("user_id")
         profile_username = get_username(friend_id)
         if not user_id:
@@ -222,13 +300,13 @@ def create_routes(app):
         if not friend_id:
             return redirect("/profile/{{ session['username'] }}", error="Virheellinen kavereista poisto")
 
-        if delete_friend(user_id, friend_id):
-            return redirect(f"/profile/{profile_username}")
-        else:
-            return render_template("profile.html", error="Käyttäjää ei voitu poistaa kavereista")
+        delete_friend(user_id, friend_id)
+        return redirect(f"/profile/{profile_username}")
 
     @app.route("/accept_friend_request/<request_username>", methods=["POST"])
     def accept_friend_request(request_username):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
         user_id = session.get("user_id")
         if not user_id:
             return redirect("/login")
@@ -240,9 +318,10 @@ def create_routes(app):
         add_friends(user_id, friend_id)
         return redirect("/friends")
 
-
     @app.route("/decline_friend_request/<request_username>", methods=["POST"])
     def decline_friend_request(request_username):
+        if session.get("csrf_token") != request.form.get("csrf_token"):
+            abort(403)
         user_id = session.get("user_id")
         if not user_id:
             return redirect("/login")

5a8c0b5bdc26d3288c6368d5f25623260074bb96
Toukka1107 <161489333+Toukka1107@users.noreply.github.com>
2024-08-31 17:59:56 +0300

    Removed unnecessary code and avatars work properly

--- friends.py
+++ friends.py
@@ -6,13 +6,8 @@ def send_request(user_id, friend_id):
         INSERT INTO friend_requests (sender_id, receiver_id)
         VALUES (:sender_id, :receiver_id)
         """)
-    try:
-        db.session.execute(sql, {"sender_id": user_id, "receiver_id": friend_id})
-        db.session.commit()
-        return True
-    except:
-        db.session.rollback()
-        return False
+    db.session.execute(sql, {"sender_id": user_id, "receiver_id": friend_id})
+    db.session.commit()
 
 def delete_friend_request(sender_id: int, receiver_id: int):
     sql = text("""
@@ -22,19 +17,13 @@ def delete_friend_request(sender_id: int, receiver_id: int):
     db.session.execute(sql, {"sender_id": sender_id, "receiver_id": receiver_id})
     db.session.commit()
 
-def get_friend_request(sender_id: int, receiver_id: int):
-    sql = text("""
-        SELECT fr.id FROM friend_requests fr
-        WHERE sender_id = :sender_id AND receiver_id = :receiver_id
-        """)
-    return db.session.execute(sql, {"sender_id": sender_id, "receiver_id": receiver_id}).fetchone()
-
 def get_all_friend_requests(receiver_id: int):
     sql = text("""
-        SELECT u.username
+        SELECT u.username, p.avatar
         FROM friend_requests fr
         JOIN users u ON u.id = fr.sender_id
-        WHERE fr.receiver_id = :receiver_id
+        JOIN profiles p ON p.user_id = fr.sender_id
+        WHERE fr.receiver_id = :receiver_id       
         """)
     return db.session.execute(sql, {"receiver_id": receiver_id}).fetchall()
 
@@ -52,32 +41,25 @@ def add_friends(user_id: int, friend_id: int):
         WHERE sender_id = :sender_id AND receiver_id = :receiver_id
         """)
     db.session.execute(sql, {"sender_id": friend_id, "receiver_id": user_id})
-
     db.session.commit()
 
 def delete_friend(user_id: int, friend_id: int):
     sql = text("""
-               DELETE FROM friends
-               WHERE user_id = :user_id AND friend_id = :friend_id
-               """)
+        DELETE FROM friends
+        WHERE user_id = :user_id AND friend_id = :friend_id
+        """)
     
-    try:
-        db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
-        db.session.execute(sql, {"user_id": friend_id, "friend_id": user_id})
-        db.session.commit()
-        return True
-    except:
-        db.session.rollback()
-        return False
-
+    db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+    db.session.execute(sql, {"user_id": friend_id, "friend_id": user_id})
+    db.session.commit()
 
 def get_all_friends(user_id: int):
     sql = text("""
-        SELECT DISTINCT u.username
+        SELECT DISTINCT u.username, p.avatar
         FROM friends f
-        JOIN users u ON (f.user_id = u.id OR f.friend_id = u.id)
-        WHERE (f.user_id = :user_id OR f.friend_id = :user_id)
-        AND u.id != :user_id
+        JOIN users u ON (u.id = f.friend_id) 
+        JOIN profiles p ON (p.user_id = f.friend_id)
+        WHERE f.user_id = :user_id
         """)
     return db.session.execute(sql, {"user_id": user_id}).fetchall()
 
@@ -87,22 +69,14 @@ def check_friend_request(user_id: int, friend_id: int):
         FROM friend_requests
         WHERE sender_id = :user_id AND receiver_id = :friend_id
         """)
-    result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
-    if result:
-        return True
-    else:
-        return None
+    return db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
 
 def check_friendship(user_id: int, friend_id: int):
     sql = text("""
         SELECT 1
         FROM friends
         WHERE user_id = :user_id AND friend_id = :friend_id
-               OR user_id = :friend_id AND friend_id = :user_id
+        OR user_id = :friend_id AND friend_id = :user_id
         """)
 
-    result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
-    if result:
-        return True
-    else:
-        return None
+    return db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id}).fetchone()
